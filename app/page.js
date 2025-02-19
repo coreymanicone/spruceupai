cd6cfec069a87a0751f06a00db9cc5dfa25fdea2
Corey Manicone <corey@Coreys-MacBook-Air-2.local>
2025-02-19 14:38:39 -0700

    Fix Vercel routing issue

--- app/page.js
+++ app/page.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Welcome to SpruceUpAI 🎉</h1>
-      <p>This is the homepage.</p>
-    </div>
-  );
+  return <h1>Welcome to SpruceUpAI 🎉</h1>;
 }
